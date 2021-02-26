4627c6a286aa08b15512184a93db1d7c193cadb5
Alexander Baranov <Caseynorth182@gmail.com>
2021-02-26 04:55:57 -0800

    add contact page

--- wp-content/themes/Lipskih/header.php
+++ wp-content/themes/Lipskih/header.php
@@ -6,14 +6,18 @@
 	<meta name="description" content="Creative Portfolio HTML Template">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Mr+De+Haviland&display=swap" rel="stylesheet">
 
 	<?php wp_head();?>
 </head>
 <body data-theme="dark">
 <header class="main-header js-main-header auto-hide-header full-slider">
 	<div class="main-header__layout">
-		<div class="main-header__logo">
-			<?php the_custom_logo();?>
+		<div class="main-header__logo main-header__logo--text">
+			<?php /*the_custom_logo();*/?>
+			<a href=""></a>
+			Lipskhyh ART
 		</div>
 		<button class="btn btn--subtle main-header__nav-trigger js-main-header__nav-trigger" aria-label="Toggle menu" aria-expanded="false" aria-controls="main-header-nav"><i class="main-header__nav-trigger-icon" aria-hidden="true"></i><span>menu</span></button>
 		<nav class="main-header__nav js-main-header__nav" id="main-header-nav" aria-labelledby="primary-menu">
